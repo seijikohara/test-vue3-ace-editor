5a5920684dbbf042cc1fff7c08d2ded328c87bda
Seiji Kohara <sage.0118@gmail.com>
2020-10-21 15:17:39 +0900

    install ace editor component

---
+++ src/views/Editor.vue
@@ -0,0 +1,21 @@
+<template>
+  <VAceEditor v-model="state.content" lang="html" theme="chrome" />
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive } from "vue";
+import { VAceEditor } from "vue3-ace-editor";
+
+export default defineComponent({
+  name: "Editor",
+  components: { VAceEditor },
+  setup() {
+    const state = reactive({
+      content: "content text"
+    });
+    return {
+      state
+    };
+  }
+});
+</script>
